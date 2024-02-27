fb7ce8a0f670c93016743495f301b9bd33d0d16b
QuocBinh1 <BinhPlayll@gmail.com>
2024-02-27 12:04:34 +0700

    update URL

--- pythonweb/urls.py
+++ pythonweb/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path , include
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='home/')),
     path('home/' ,     include('home.urls')),
     path('register/' , include('register.urls')),
     path('login/'    , include('login.urls')),
